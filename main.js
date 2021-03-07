ecc65d85e9e5eb2a6d4a4a406f86531bfeaf53d1
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-03-07 01:11:49 +0100

    Changed the background image

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const minutesCount = document.getElementById("mins");
 const secondsCount = document.getElementById("seconds");
 
 // Get the End Date
-const endDate = "Jan 1, 2022";
+const endDate = "Jan 1 2022";
 
 const challengeCountDown = () => {
   const challengeEndDate = new Date(endDate).getTime(); // End Date
